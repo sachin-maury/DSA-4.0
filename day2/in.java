a1c92761174a62e4c67fc9b1274be43793f7d71c
Sachin Maurya <kumarsachin67671@gmail.com>
2024-11-05 18:27:59 +0530

    input by user

---
+++ day2/in.java
@@ -0,0 +1,12 @@
+package day2;
+
+import java.util.Scanner;
+
+public class in {
+    public static void main(String[] args) {
+        System.out.println("Enter your number :");
+        Scanner input = new Scanner(System.in);
+        int rollnm = input.nextInt();
+        System.out.println("The roll Number is :" +rollnm);
+    }
+}
