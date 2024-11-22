a17e64864df83618bdc20545fee092ce2e2b8bde
Sachin Maurya <kumarsachin67671@gmail.com>
2024-11-22 09:19:19 +0530

    practice

---
+++ day5/practice.java
@@ -0,0 +1,13 @@
+package day5;
+
+public class practice {
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+        System.out.println("Your most welcom "+name);
+    
+}
+
+}
