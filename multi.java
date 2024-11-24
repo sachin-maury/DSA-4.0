47bfb32323bee3ae0ccd7fcb41e6317b7fc8bb56
Sachin Maurya <kumarsachin67671@gmail.com>
2024-11-24 11:11:16 +0530

    multi

---
+++ multi.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class multi {
+     public static void main(String[] args) {
+        Scanner sachin = new Scanner(System.in);
+        System.out.println("Enter your number :");
+        int num = sachin.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println( (num * i));
+        }
+
+    }
+    
+}
