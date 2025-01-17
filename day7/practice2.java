b5ba2dc0d7bbbc7ac59721f8ae6ad17650190252
Sachin Maurya <kumarsachin67671@gmail.com>
2025-01-17 09:18:08 +0530

    Simple interest

---
+++ day7/practice2.java
@@ -0,0 +1,9 @@
+public class practice2 {
+    public static void main(String[] args) {
+      int P =20000;
+      int R =5;
+      int T = 4;
+      int Si =(P*R*T)/100;
+      System.out.println("The Simple interest is : "+Si); 
+    }
+}
