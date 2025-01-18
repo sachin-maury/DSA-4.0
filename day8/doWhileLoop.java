d95fec789e5a8d8f213121bd04d04e9dc809b3eb
Sachin Maurya <kumarsachin67671@gmail.com>
2025-01-18 20:39:24 +0530

    do while loop

---
+++ day8/doWhileLoop.java
@@ -0,0 +1,12 @@
+
+
+public class doWhileLoop {
+    public static void main(String[] args) {
+        int num = 1;
+        do{
+            System.out.println(num);
+            num ++;
+        }
+        while(num<=5);
+    }
+}
