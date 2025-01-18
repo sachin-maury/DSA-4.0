db62f8023b7e523197d9677a02273ffdc64bc7c1
Sachin Maurya <kumarsachin67671@gmail.com>
2025-01-18 20:30:27 +0530

    while loop java

---
+++ day8/whileLoop.java
@@ -0,0 +1,13 @@
+//package day8;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        int num = 1;
+        
+        while(num<=5){
+            System.out.println(num);
+           num += 1;
+        }
+        
+    }
+}
