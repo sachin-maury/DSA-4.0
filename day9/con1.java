988b2fec7ec29b042c77cb5e8f77520ad6534d82
Sachin Maurya <kumarsachin67671@gmail.com>
2025-01-19 10:31:37 +0530

    character program

---
+++ day9/con1.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class con1 {
+    public static void main(String[] args) {
+     //   Scanner in = new Scanner(System.in);
+
+        String name = "Sachin";
+        System.out.println(name.charAt(1));
+    }
+}
