7b483fdc0f57fef0ba0b230980c26391e93d9ab0
Sachin Maurya <kumarsachin67671@gmail.com>
2025-01-22 08:10:10 +0530

    simple method program

---
+++ day12/method1.java
@@ -0,0 +1,12 @@
+
+
+class method1 {
+ public static void main(String[] args) {
+    String message = greet();
+    System.out.println(message);
+ }
+ public static String greet(){
+        String str ="Sachin";
+        return str;
+}
+}
