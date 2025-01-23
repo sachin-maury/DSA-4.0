e4649dd35e29a980db03e68b456b692f3051e5ac
Sachin Maurya <kumarsachin67671@gmail.com>
2025-01-23 21:30:46 +0530

    method

---
+++ day12/method2.java
@@ -0,0 +1,13 @@
+public class method2 {
+    public static void main(String[] args) {
+        
+       String message = greet();
+       System.out.println(message);
+    }
+    public static String greet(){
+           String str ="Manish";
+           return str;
+   }
+   }
+
+
