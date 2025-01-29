d88ca352bf1008f5d01b15f5bb3503cf9389c842
Sachin Maurya <kumarsachin67671@gmail.com>
2025-01-29 07:54:02 +0530

    method problem

---
+++ day14/method1.java
@@ -0,0 +1,15 @@
+
+
+import java.util.Arrays;
+
+public class method1 {
+    
+    static void chang(int[] nums){
+        nums[0]=99;
+    }
+    public static void main(String[] args) {
+        int arr[]= {1,3,2,45,6};
+        chang(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
