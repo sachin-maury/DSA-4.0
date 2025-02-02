237e525c1469f87dd8e10657eaab69bf7091256b
Sachin Maurya <kumarsachin67671@gmail.com>
2025-02-02 22:29:50 +0530

    practice array

---
+++ day15/arrayinput.java
@@ -0,0 +1,25 @@
+
+
+import java.util.Scanner;
+
+import java.util.*;
+
+public class arrayinput {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[] = new int[5];
+        arr[0] =99;
+        arr[1] = 54;
+        arr[2] = 75;
+        arr[3] = 14;
+        arr[4] = 53;
+        System.out.println(arr[4]);
+        for(int i=0; i<arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+        for(int i =0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        
+    }
+}
