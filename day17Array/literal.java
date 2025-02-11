aea4de868e6b2d38406ff5301bae7a2c4e9371ce
Sachin Maurya <kumarsachin67671@gmail.com>
2025-02-11 23:46:11 +0530

    Array Literals

---
+++ day17Array/literal.java
@@ -0,0 +1,11 @@
+
+
+public class literal {
+    public static void main(String[] args) {
+        String [] arr = new String[3];
+        String [] names = {"Sachin","Shakti","Sadanand"};
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+    }
+}
