bdb9201cc47b95e6f03adf62d7a6a6f9ea84cfb9
Sachin Maurya <kumarsachin67671@gmail.com>
2025-02-12 00:00:33 +0530

    2D Array

---
+++ day17Array/arr2d.java
@@ -0,0 +1,27 @@
+
+public class arr2d {
+public static void MultiArrays(){
+    //int [][]arr= new int [][]
+    int [][] arr= {{45,12,6},{58,65,4},{74,14,2}};
+
+    System.out.println(arr[0][0]);//45
+    System.out.println(arr[0][1]);//12
+    System.out.println(arr[0][2]);//6
+
+    System.out.println(arr[1][0]);//58
+    System.out.println(arr[1][1]);//65
+    System.out.println(arr[1][2]);//4
+
+    System.out.println(arr[2][0]);//74
+    System.out.println(arr[2][1]);//14
+    System.out.println(arr[2][2]);//2
+}
+
+
+
+    public static void main(String[] args) {
+        arr2d obj = new arr2d();
+        obj.MultiArrays();
+    }
+    
+}
