63bce0fa7cad73a0b6c3c6643e1ace64592bb11a
Sachin Maurya <kumarsachin67671@gmail.com>
2025-02-12 23:25:34 +0530

    array

---
+++ day17Array/tempCodeRunnerFile.java
@@ -0,0 +1,7 @@
+ // System.out.println(arr[1][0][0]);//25
+        // System.out.println(arr[1][1][1]);//45
+        // System.out.println(arr[1][2][2]);//14
+
+        // System.out.println(arr[2][0][0]);//47
+        // System.out.println(arr[2][1][1]);//54
+        // System.out.println(arr[2][2][2]);//21
