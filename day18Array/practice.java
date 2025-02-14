0fc3d873c6e85413079d1c58bf5709e41584909b
Sachin Maurya <kumarsachin67671@gmail.com>
2025-02-14 23:20:31 +0530

    practice

---
+++ day18Array/practice.java
@@ -0,0 +1,14 @@
+public class practice {
+
+    public static void DemoArr(){
+      //  int arr[] = new int[3];
+      int arr[]={25,36,14};
+      System.out.println(arr.length);
+    }
+    public static void main(String[] args) {
+        lengthArr obj = new lengthArr();
+        obj.DemoArr();
+
+    }
+}
+
