29895106f68df95008f32c5729e8788578742280
Sachin Maurya <kumarsachin67671@gmail.com>
2025-02-15 21:32:14 +0530

    loop in Array

---
+++ day19Array/loopArray.java
@@ -0,0 +1,20 @@
+
+public class looparray {
+
+    void demoArr(){
+        int arr[] = new int[5];
+            arr[0] =22;
+            arr[1] =24;
+            arr[2] =26;
+
+        for(int i =0; i<3; i++){
+            System.out.println(i);
+        }
+    }
+
+}
+    public static void main(String[] args) {
+        looparray obj = new looparray();
+        obj.demoArr();
+    }
+
