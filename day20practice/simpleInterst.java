39c5e2bb739e69a5d9c2ac0484af48146100793c
Sachin Maurya <kumarsachin67671@gmail.com>
2025-02-16 14:07:45 +0530

    Simple interset using method

---
+++ day20practice/simpleInterst.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+
+public class simpleInterst {
+
+    void SimpleValue(int simpleCal){
+        
+        System.out.println("The simple interest is : "+simpleCal);
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter your princlple : ");
+        Scanner in = new Scanner(System.in);
+        int P= in.nextInt();
+        System.out.println("Enter your precentage : ");
+        int R = in.nextInt();
+        System.out.println("Enter your Time : ");
+        int T = in.nextInt();
+        int simpleCal= (P*R*T)/100;
+        simpleInterst obj = new simpleInterst();
+        obj.SimpleValue(simpleCal);
+        
+    }
+}
