3a23fff8839eb21b96960318f7045ace10253bd7
Sachin Maurya <kumarsachin67671@gmail.com>
2025-02-16 13:46:12 +0530

    Leap Year with method

---
+++ day20practice/leapyear.java
@@ -0,0 +1,26 @@
+
+
+import java.util.Scanner;
+
+public class leapyear {
+
+    void LeapYears(int year){
+        if (year % 4==0 && year % 100 != 0 || year %400==0){
+            System.out.println(year+"This is a leap year:");
+            
+        }
+        else{
+            System.out.println(year+" This is not leap year:");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Enter your Year: ");
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        leapyear obj = new leapyear();
+        obj.LeapYears(year);
+         
+    }
+}
