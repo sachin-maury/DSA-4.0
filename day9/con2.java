edb9a006ff0a6e6b0388f02a95a8332d558c58bf
Sachin Maurya <kumarsachin67671@gmail.com>
2025-02-24 23:07:41 +0530

    practice

---
+++ day9/con2.java
@@ -0,0 +1,13 @@
+public class con2 {
+    
+
+
+    public static void main(String[] args) {
+     //   Scanner in = new Scanner(System.in);
+
+        String name = "Sachin";
+        System.out.println(name.charAt(1));
+    }
+}
+
+}
