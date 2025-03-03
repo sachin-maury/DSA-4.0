86c947b8765973ab41a3b8d6055ee3578603d509
Sachin Maurya <kumarsachin67671@gmail.com>
2025-03-03 23:25:49 +0530

    practice

---
+++ day8/practice2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class practice2 {
+     public static void main(String[] args) {
+    Scanner sachin = new Scanner(System.in);
+    System.out.println("Enter your number in celsious:");
+    float tempC = sachin.nextFloat();
+    float tempF = (tempC*9/5)+32;
+    System.out.println(tempF);
+}
