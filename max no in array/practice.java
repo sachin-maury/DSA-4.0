49ce1673d843065f9fcd48bf9240a59e580dd33b
Sachin Maurya <kumarsachin67671@gmail.com>
2025-03-07 08:03:04 +0530

    practice

---
+++ max no in array/practice.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class practice {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter your number : ");
+        for(int i =0 ; i<arr.length; i++){
+           arr[i] = sc.nextInt();
+
+        }
+    }
+}
