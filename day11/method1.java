84cb41da54b518f4f8cb8148c47067953350e51f
Sachin Maurya <kumarsachin67671@gmail.com>
2025-03-17 23:26:20 +0530

    practice

---
+++ day11/method1.java
@@ -0,0 +1,14 @@
+
+public class method1 {
+    public static void main(String[] args) {
+        String personalised = myGreat();
+    }
+    static String myGreat(String greet){
+        String message = "hello sachin!"+greet;
+        return message;
+    }
+    static String greet(){
+        String greeting = "how are you";
+        return greeting;
+    }
+}
