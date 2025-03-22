daac5c70b2a500b099bd2b1e290f6ec08fb87c3a
Sachin Maurya <kumarsachin67671@gmail.com>
2025-03-22 22:57:52 +0530

    practice

--- day5/greeting2.java
+++ day5/greeting2.java
@@ -3,6 +3,9 @@ package day5;
 import java.util.Scanner;
 
 public class greeting2 {
+
+
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name:");
