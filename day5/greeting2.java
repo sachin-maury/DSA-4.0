6167e41a864f18e09d72fabeb50e6b1c08694cd7
Sachin Maurya <kumarsachin67671@gmail.com>
2025-03-31 21:38:58 +0530

    pracitce

--- day5/greeting2.java
+++ day5/greeting2.java
@@ -7,7 +7,6 @@ public class greeting2 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name:");
         String name = sc.nextLine();
-        
         System.out.println("Your most welcom "+name);
 
         
