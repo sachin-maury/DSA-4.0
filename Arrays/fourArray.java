e28562fe5821f552ffcd3e3d9579363288989baf
Sachin Maurya <kumarsachin67671@gmail.com>
2025-05-02 10:13:28 +0530

    length of the arr

---
+++ Arrays/fourArray.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class fourArray {
+    void LengthArr(){
+        int arr[] = {5,6,4,8,9};
+        System.out.println(arr.length);
+    }
+    
+    
+    public static void main(String[] args) {
+        fourArray obj = new fourArray();
+        obj.LengthArr();
+    }
+}
