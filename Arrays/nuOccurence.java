84e5ecc9df7cf79bf90a1026a30c7571d590af76
Sachin Maurya <kumarsachin67671@gmail.com>
2025-05-04 11:55:11 +0530

    Count Occurence of number x.

---
+++ Arrays/nuOccurence.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class nuOccurence {
+
+    static int OccurenceElement(int arr[] , int x){
+        int count = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==x){
+                count++;
+            }
+        }
+        return count;
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size Of Array"); 
+        int n = sc.nextInt();
+        
+        int arr[] = new int[n];
+        System.out.println("Enter "+n+" element");
+        for(int i=0; i<n; i++){
+            arr[i]= sc.nextInt(); 
+        }
+        System.out.println("Enter X");
+        int x = sc.nextInt();
+        
+        System.out.println("Count Of "+OccurenceElement(arr, x));
+       
+    }
+}
