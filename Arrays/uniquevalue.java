ac2e9b2a4ccee571352aa64d9ca1e86aa07c316c
Sachin Maurya <kumarsachin67671@gmail.com>
2025-05-11 23:09:22 +0530

    uniqevalue

--- Arrays/uniquevalue.java
+++ Arrays/uniquevalue.java
@@ -30,7 +30,6 @@ public class uniquevalue {
         System.out.println("Enter "+n+" element");
         for(int i=0;i<arr.length; i++){
             arr[i]= sc.nextInt();
-            
         }
         System.out.println("Unique element"+findUnique(arr));       
     }
