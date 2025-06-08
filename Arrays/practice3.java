3d507d687d467a8d404a452b06e312d7d3401ecf
Sachin Maurya <kumarsachin67671@gmail.com>
2025-06-08 23:02:38 +0530

    practice

---
+++ Arrays/practice3.java
@@ -0,0 +1,14 @@
+import day2.in;
+
+public class practice3 {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        arr[0]= 55;
+        arr[1]=56;
+        arr[2]=57;
+        arr[3]=58;
+        arr[4]=60;
+
+        System.out.println(arr[0]);
+    }
+}
