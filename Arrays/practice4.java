39d08d8e7fee0f2f5c72e342fdf136b26010d475
Sachin Maurya <kumarsachin67671@gmail.com>
2025-06-13 12:57:14 +0530

    pra

--- Arrays/practice4.java
+++ Arrays/practice4.java
@@ -2,6 +2,7 @@ public class practice4 {
     public static void main(String[] args) {
         System.out.println("hii");
         System.out.println("How are you");
+        System.out.println("hii");
 
 
     }
