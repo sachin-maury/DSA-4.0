84f6d8231c82ef653151209265dd8f2719ba32d6
Sachin Maurya <kumarsachin67671@gmail.com>
2025-06-18 20:21:44 +0530

    practice

---
+++ Arrays/practice5.java
@@ -0,0 +1,11 @@
+public class practice5 {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        arr[0] = 50;
+        arr[1] = 12;
+        arr[2] = 43;
+        arr[3] = 23;
+        arr[4] = 63;
+        System.out.println(arr[2]);
+    }
+}
