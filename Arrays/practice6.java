732a7fbc2cef5ac08705f2658f062d32c1e14cc9
Sachin Maurya <kumarsachin67671@gmail.com>
2025-06-20 07:43:52 +0530

    practice

---
+++ Arrays/practice6.java
@@ -0,0 +1,15 @@
+public class practice6 {
+
+    void Run(){
+        int a =40;
+        int b= 30;
+        int c = a+b;
+        System.out.println(c);
+        
+    }
+    public static void main(String[] args) {
+        practice6 obj = new practice6();
+        obj.Run();
+        
+    }
+}
