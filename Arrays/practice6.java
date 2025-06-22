0147485827706785dbdee7f6afa63051576680d8
Sachin Maurya <kumarsachin67671@gmail.com>
2025-06-22 22:03:08 +0530

    p

--- Arrays/practice6.java
+++ Arrays/practice6.java
@@ -3,8 +3,9 @@ public class practice6 {
     void Run(){
         int a =50;
         int b= 30;
-        int c = a+b;
-        System.out.println(c);
+        int c=50;
+        int d = a+b-c;
+        System.out.println(d);
         
     }
     public static void main(String[] args) {
