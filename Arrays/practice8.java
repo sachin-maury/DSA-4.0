0930b8edac4b21c8f3e6250c57530e1abb9e0438
Sachin Maurya <kumarsachin67671@gmail.com>
2025-07-02 23:15:05 +0530

    pracitce

---
+++ Arrays/practice8.java
@@ -0,0 +1,17 @@
+public class practice8 {
+ 
+     void Run(){
+        int a =50;
+        int b= 30;
+        int c=50;
+        int d = a+b-c;
+        System.out.println(d);
+        
+    }
+    public static void main(String[] args) {
+        practice6 obj = new practice6();
+        obj.Run();
+        
+    }
+  }
+
