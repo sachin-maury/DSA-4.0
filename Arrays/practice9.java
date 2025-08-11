a0c591922ecbc9cf9ae82a8c6fbcd2fb66a9fd86
Sachin Maurya <kumarsachin67671@gmail.com>
2025-08-11 23:00:09 +0530

    practice

--- Arrays/practice9.java
+++ Arrays/practice9.java
@@ -8,6 +8,7 @@ public class practice9 {
       System.out.println(num[0][0]);
       System.out.println(num[0][1]);
       System.out.println(num[0][2]);
+      System.out.println(num[0][2]);
 
 
       System.out.println(num[1][0]);
