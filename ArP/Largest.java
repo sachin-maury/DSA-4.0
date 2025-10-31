f27960c7384a7e0d3b4672f19f531c76cf0b2133
Sachin Maurya <kumarsachin67671@gmail.com>
2025-10-31 22:29:32 +0530

    Largest element in the array

---
+++ ArP/Largest.java
@@ -0,0 +1,14 @@
+package ArP;
+
+public class Largest {
+    public static void main(String[] args) {
+        int a[] = {1,2,3,4,5};
+        int largest = a[0];
+        for ( int i = 0; i< a.length; i++){
+            if (a[i]>largest) {
+                largest= a[i];
+            }
+        }
+        System.out.println("The Largest element is : "+ largest);
+    }
+}
